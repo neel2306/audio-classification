3d9ebdc9b41174af5b4a8784583b19092c0b95fd
Neelabh Somani <83975955+neel2306@users.noreply.github.com>
2022-12-16 12:31:23 +0530

    Keyword Spotter class that identifies the speech command given

--- system.py
+++ system.py
@@ -78,12 +78,3 @@ class _Keyword_Spotter_:
         print("Finished recording!......")
         return audio_file
 
-if __name__=="__main__":
-    spotter_instance = _Keyword_Spotter_()
-    
-    #Recording an audio file.
-    audio_file = spotter_instance.record(_SAMPLES_, _SECONDS_)
-    #Prediction.
-    MFCCs = spotter_instance.preprocess(audio_file)
-    keyword = spotter_instance.prediction(MFCCs)
-    print(f'The keyword is: {keyword}')
